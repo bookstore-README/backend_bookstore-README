56997975dd1aa70fee98ac260c33e3a5503030f3
Mun Kyoung-min <mun9659@naver.com>
2024-01-20 19:02:21 +0900

    feat: Member 회원가입

--- src/main/java/com/bookstore/readme/domain/member/api/MemberApi.java
+++ src/main/java/com/bookstore/readme/domain/member/api/MemberApi.java
@@ -1,4 +1,25 @@
 package com.bookstore.readme.domain.member.api;
 
+import com.bookstore.readme.domain.member.application.MemberService;
+import com.bookstore.readme.domain.member.dto.MemberDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
 public class MemberApi {
+
+    @Autowired
+    private MemberService memberService;
+
+    @PostMapping("/sign-up")
+    public void save(MemberDto memberDto) {
+
+        memberService.save(memberDto);
+
+    }
+
 }
--- src/main/java/com/bookstore/readme/domain/member/domain/Member.java
+++ src/main/java/com/bookstore/readme/domain/member/domain/Member.java
@@ -3,14 +3,20 @@ package com.bookstore.readme.domain.member.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Builder
 public class Member {
     @Id
     @GeneratedValue
     private Long id;
 
     private String name;
+    private String nickname;
+    private String profileImage;
     private String email;
     private String password;
 }
