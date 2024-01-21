5ea9be6837be10c98341c30cdbe1a0e26840934b
Mun Kyoung-min <mun9659@naver.com>
2024-01-21 21:00:52 +0900

    Revert "[#9, #10] Spring Security, Swagger 기본 구성"

--- src/main/java/com/bookstore/readme/domain/member/api/MemberApi.java
+++ src/main/java/com/bookstore/readme/domain/member/api/MemberApi.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-// @RequestMapping("/member")
+@RequestMapping("/member")
 public class MemberApi {
 
     private final MemberService memberService;
