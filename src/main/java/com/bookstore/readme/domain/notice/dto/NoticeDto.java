0ed6686c0f2259f47feb317d225d8d062475c92f
didado <idejrdud@gmail.com>
2024-01-31 19:01:14 +0900

    fix: 게시글 Response 데이터에 생성날짜, 수정날짜 추가

--- src/main/java/com/bookstore/readme/domain/notice/dto/NoticeDto.java
+++ src/main/java/com/bookstore/readme/domain/notice/dto/NoticeDto.java
@@ -3,17 +3,29 @@ package com.bookstore.readme.domain.notice.dto;
 import com.bookstore.readme.domain.notice.domain.Notice;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class NoticeDto {
     private final String title;
     private final String content;
 
-    public NoticeDto(String title, String content) {
+    private final LocalDateTime createDate;
+    private final LocalDateTime updateDate;
+
+    public NoticeDto(String title, String content, LocalDateTime createDate, LocalDateTime updateDate) {
         this.title = title;
         this.content = content;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
     }
 
     public static NoticeDto of(Notice notice) {
-        return new NoticeDto(notice.getTitle(), notice.getContent());
+        return new NoticeDto(
+                notice.getTitle(),
+                notice.getContent(),
+                notice.getCreateDate(),
+                notice.getUpdateDate()
+        );
     }
 }
--- src/main/java/com/bookstore/readme/domain/notice/dto/NoticeSearchDto.java
+++ src/main/java/com/bookstore/readme/domain/notice/dto/NoticeSearchDto.java
@@ -3,16 +3,24 @@ package com.bookstore.readme.domain.notice.dto;
 import com.bookstore.readme.domain.notice.domain.Notice;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class NoticeSearchDto extends NoticeDto {
     private final Long noticeId;
 
-    public NoticeSearchDto(Long noticeId, String title, String content) {
-        super(title, content);
+    public NoticeSearchDto(String title, String content, LocalDateTime createDate, LocalDateTime updateDate, Long noticeId) {
+        super(title, content, createDate, updateDate);
         this.noticeId = noticeId;
     }
 
     public static NoticeSearchDto of(Notice notice) {
-        return new NoticeSearchDto(notice.getId(), notice.getTitle(), notice.getContent());
+        return new NoticeSearchDto(
+                notice.getTitle(),
+                notice.getContent(),
+                notice.getCreateDate(),
+                notice.getUpdateDate(),
+                notice.getId()
+        );
     }
 }
