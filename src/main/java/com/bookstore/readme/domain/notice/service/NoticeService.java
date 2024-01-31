f60cc4f347a5c21016e53f1182dedb52f5d8bf69
didado <idejrdud@gmail.com>
2024-01-31 16:28:35 +0900

    feat: 게시글 전체 조회 기능 추가

--- src/main/java/com/bookstore/readme/domain/notice/service/NoticeService.java
+++ src/main/java/com/bookstore/readme/domain/notice/service/NoticeService.java
@@ -9,24 +9,33 @@ import com.bookstore.readme.domain.notice.response.NoticeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
     private final NoticeRepository noticeRepository;
 
-    public Long save(NoticeSaveRequest request) {
-        Notice notice = request.toNotice();
-        noticeRepository.save(notice);
-        return notice.getId();
-
-
-    }
-
-    public NoticeResponse searchNotice(Long noticeId) {
+    public NoticeResponse getNotice(Long noticeId) {
         Notice notice = noticeRepository.findById(noticeId)
                 .orElseThrow(() -> new NotFoundNoticeByIdException(noticeId));
 
         NoticeSearchDto noticeSearchDto = NoticeSearchDto.of(notice);
         return NoticeResponse.ok(noticeSearchDto);
+    }
+
+    public NoticeResponse getNotices() {
+        List<Notice> notices = noticeRepository.findAll();
+        List<NoticeSearchDto> convertNotices = notices.stream()
+                .map(NoticeSearchDto::of)
+                .toList();
+
+        return NoticeResponse.ok(convertNotices);
+    }
+
+    public Long save(NoticeSaveRequest request) {
+        Notice notice = request.toNotice();
+        noticeRepository.save(notice);
+        return notice.getId();
     }
 }
