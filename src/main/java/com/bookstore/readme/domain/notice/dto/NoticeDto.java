cb55d38e4c343917239713456d692019b351030a
didado <idejrdud@gmail.com>
2024-01-31 16:47:02 +0900

    fix: 게시판 단일조회, 전체조회 DTO 클래스 분리

---
+++ src/main/java/com/bookstore/readme/domain/notice/dto/NoticeDto.java
@@ -0,0 +1,20 @@
+package com.bookstore.readme.domain.notice.dto;
+
+import com.bookstore.readme.domain.notice.domain.Notice;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class NoticeDto {
+    private final String title;
+    private final String content;
+
+    public NoticeDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public static NoticeDto of(Notice notice) {
+        return new NoticeDto(notice.getTitle(), notice.getContent());
+    }
+}
--- src/main/java/com/bookstore/readme/domain/notice/dto/NoticeSearchDto.java
+++ src/main/java/com/bookstore/readme/domain/notice/dto/NoticeSearchDto.java
@@ -1,24 +1,16 @@
 package com.bookstore.readme.domain.notice.dto;
 
 import com.bookstore.readme.domain.notice.domain.Notice;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class NoticeSearchDto {
-    private final String title;
-    private final String content;
+public class NoticeSearchDto extends NoticeDto {
 
-    @Builder
     public NoticeSearchDto(String title, String content) {
-        this.title = title;
-        this.content = content;
+        super(title, content);
     }
 
     public static NoticeSearchDto of(Notice notice) {
-        return NoticeSearchDto.builder()
-                .title(notice.getTitle())
-                .content(notice.getContent())
-                .build();
+        return new NoticeSearchDto(notice.getTitle(), notice.getContent());
     }
 }
