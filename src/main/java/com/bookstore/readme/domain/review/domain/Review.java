e1deb85076302f5ff8e53d72ce3f0e658ffc7842
didado <idejrdud@gmail.com>
2024-02-01 18:06:04 +0900

    fix: 리뷰 업데이트 날짜 적용 안되는 에러 수정

--- src/main/java/com/bookstore/readme/domain/review/domain/Review.java
+++ src/main/java/com/bookstore/readme/domain/review/domain/Review.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -31,7 +32,7 @@ public class Review {
     @Column(updatable = false)
     private LocalDateTime createDate;
 
-    @CreatedDate
+    @LastModifiedDate
     @Column(updatable = true)
     private LocalDateTime updateDate;
 
