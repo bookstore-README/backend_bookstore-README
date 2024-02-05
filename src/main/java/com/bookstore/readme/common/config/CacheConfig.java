45e1c38285c05012cb09a5036e5d59dac24b0727
didado <idejrdud@gmail.com>
2024-02-05 23:43:34 +0900

    feat: #74 조회수 증가 기능 추가

---
+++ src/main/java/com/bookstore/readme/common/config/CacheConfig.java
@@ -0,0 +1,35 @@
+package com.bookstore.readme.common.config;
+
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(100)
+                .recordStats();
+    }
+
+    @Bean("viewKeyGeneratorBean")
+    public KeyGenerator keyGenerator() {
+        return new ViewKeyGenerator();
+    }
+}
