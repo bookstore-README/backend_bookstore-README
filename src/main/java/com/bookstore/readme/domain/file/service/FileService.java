a5d70e802130acb541413a3ec559b0d0c7a64ebc
Mun Kyoung-min <mun9659@naver.com>
2024-02-09 23:34:54 +0900

    fix: 정적 이미지 불러오기 수정

--- src/main/java/com/bookstore/readme/domain/file/service/FileService.java
+++ src/main/java/com/bookstore/readme/domain/file/service/FileService.java
@@ -1,7 +1,6 @@
 package com.bookstore.readme.domain.file.service;
 
 import com.bookstore.readme.domain.file.exception.NotEqualFileExt;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,7 +40,7 @@ public class FileService {
             e.printStackTrace();
         }
 
-        return filePath + saveName;
+        return "/uploadImage/" + saveName;
     }
 
     private String createFileName(String originFileName) {
