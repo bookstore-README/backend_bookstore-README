ed18ff47d7a9ebc3354cecbb0727726881b7506e
Mun Kyoung-min <mun9659@naver.com>
2024-02-20 21:57:53 +0900

    feat: Delivery(배송) 조회 조건문 추가, 삭제 추가, 하위 테이블 삭제 처리 추가

--- src/main/java/com/bookstore/readme/domain/delivery/repository/DeliveryRepository.java
+++ src/main/java/com/bookstore/readme/domain/delivery/repository/DeliveryRepository.java
@@ -1,8 +1,10 @@
 package com.bookstore.readme.domain.delivery.repository;
 
 import com.bookstore.readme.domain.delivery.domain.Delivery;
+import com.bookstore.readme.domain.delivery.domain.DeliveryStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,5 +13,23 @@ public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
     List<Delivery> findByMemberId(Long memberId);
 
     Optional<Delivery> findByIdAndMemberId(Long id, Long memberId);
+
+    List<Delivery> findAllByMemberIdAndCreateDateBetween(Long memberId
+            , LocalDateTime start
+            , LocalDateTime end);
+
+    // @Query("select d from Delivery d " +
+    //         "left join Member m on m.member_id = d.member_id" +
+    //         "where m.member_id = :member_id" +
+    //         "and (:delivery_status is null or d.delivery_status = :delivery_status)" +
+    //         "and d.create_date between :start and :end"
+    // )
+    List<Delivery> findCustomByMemberIdAndDeliveryStatusAndCreateDateBetween(
+           Long memberId
+            , DeliveryStatus deliveryStatus
+            , LocalDateTime start
+            , LocalDateTime end);
+
+
 
 }
