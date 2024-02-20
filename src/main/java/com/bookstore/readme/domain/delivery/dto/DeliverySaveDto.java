fbe96bcf88d4dd2123e2c14a6e5266059243fe3c
Mun Kyoung-min <mun9659@naver.com>
2024-02-20 22:13:51 +0900

    fix: Delivery(배송) 관련 Swagger Dto 설명 추가

--- src/main/java/com/bookstore/readme/domain/delivery/dto/DeliverySaveDto.java
+++ src/main/java/com/bookstore/readme/domain/delivery/dto/DeliverySaveDto.java
@@ -4,6 +4,8 @@ import com.bookstore.readme.domain.delivery.domain.Delivery;
 import com.bookstore.readme.domain.delivery.domain.DeliveryStatus;
 import com.bookstore.readme.domain.member.model.Member;
 import com.bookstore.readme.domain.order.dto.OrderBookSaveDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.util.List;
@@ -11,12 +13,31 @@ import java.util.List;
 @Getter
 public class DeliverySaveDto {
 
+    @Schema(description = "배송 받는 사람 이름", example = "아무개")
+    @NotNull(message = "배송 받는 사람의 이름은 필수입니다.")
     private String name;
+
+    @Schema(description = "배송 받는 사람 전화번호", example = "010-0000-0000")
+    @NotNull(message = "배송 받는 사람의 이름은 필수입니다.")
     private String phone;
+
+    @Schema(description = "배송 받는 사람 주소", example = "주소 풀네임")
+    @NotNull(message = "배송 받는 사람의 이름은 필수입니다.")
     private String address;
+
+    @Schema(description = "배송 시 전달할 메시지")
     private String message;
+
+    @Schema(description = "결제 방법", example = "CARD")
+    @NotNull(message = "결제 방법은 필수입니다.")
     private String paymentMethod;
+
+    @Schema(description = "결제 금액", example = "42000")
+    @NotNull(message = "결제 금액은 필수입니다.")
     private Integer paymentAmount;
+
+    @Schema(description = "주문한 책 목록")
+    @NotNull(message = "주문한 책 목록은 필수입니다.")
     private List<OrderBookSaveDto> orderBooks;
 
     public Delivery toEntity(Member member) {
--- src/main/java/com/bookstore/readme/domain/order/dto/OrderBookSaveDto.java
+++ src/main/java/com/bookstore/readme/domain/order/dto/OrderBookSaveDto.java
@@ -2,13 +2,19 @@ package com.bookstore.readme.domain.order.dto;
 
 import com.bookstore.readme.domain.book.domain.Book;
 import com.bookstore.readme.domain.order.domain.OrderBook;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class OrderBookSaveDto {
 
+    @Schema(description = "책 고유 ID")
+    @NotNull(message = "책 고유 ID는 필수입니다.")
     Integer bookId;
 
+    @Schema(description = "주문 책 권수")
+    @NotNull(message = "주문한 책 권수는 필수입니다.")
     Integer quantity;
 
 }
