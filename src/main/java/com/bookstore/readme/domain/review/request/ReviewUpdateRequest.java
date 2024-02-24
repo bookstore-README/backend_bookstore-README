3bcfaa6250753f88557806acea9a8ec7f6203aba
didado <idejrdud@gmail.com>
2024-02-24 15:24:13 +0900

    fix: 리뷰 수정 rating 추가

--- src/main/java/com/bookstore/readme/domain/review/request/ReviewUpdateRequest.java
+++ src/main/java/com/bookstore/readme/domain/review/request/ReviewUpdateRequest.java
@@ -1,11 +1,16 @@
 package com.bookstore.readme.domain.review.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
 public class ReviewUpdateRequest extends ReviewRequest {
+    @NotNull(message = "리뷰 평점은 필수입니다.")
+    @Schema(example = "5", description = "수정할 평점")
+    private final Double rating;
 }
